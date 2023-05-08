4ee3e3ee5884a92f23f91b8ab3bba0ed08b81b85
Estudiante <antoniocalzado02@gmail.com>
2023-05-08 10:33:01 +0200

    Primera subida

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductFormComponent } from './product/product-form/product-form.component';
+
+const routes: Routes = [
+{path:'', component: HomeComponentComponent},
+
+{path: '', children:[
+  { path: '', component: ProductListComponent },
+  { path: ':id', component: ProductDetailComponent }
+]},
+
+{ 
+  path: 'products',
+  loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductModule } from './product/product.module';
+import { HomeComponentComponent } from './home-component/home-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule,
+    AppModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
